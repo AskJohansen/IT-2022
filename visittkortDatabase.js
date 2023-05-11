3f0c3335a13efd50348e6b896d331d4154c5896c
AskJohansen <113419353+AskJohansen@users.noreply.github.com>
2023-05-11 14:27:05 +0200

    Add files via upload

---
+++ visittkortDatabase.js
@@ -0,0 +1,40 @@
+let dataInfo = {
+    "kortArray": [
+        {
+            navn: "Eirik Djup",
+            kjonn: "Mann",
+            stilling: "Jobber i barnehage(frivelig)",
+            telefonnummer: "90410069",
+            valgfri: "Sindrelover69",
+            forerkort: "Har ikke førerkort",
+            fodselsdag: "2005-07-13",
+        },
+        {
+            navn: "Sindre Knapp",
+            kjonn: "Mann",
+            stilling: "Biznismann",
+            telefonnummer: "12345678",
+            valgfri: "Eiriklover69",
+            forerkort: "Har ikke førerkort",
+            fodselsdag: "2005-09-26",
+        },
+        {
+            navn: "Christian Viken-Slåtten",
+            kjonn: "Mann",
+            stilling: "Jobber med skole",
+            telefonnummer: "911",
+            valgfri: "Trykka på Q",
+            forerkort: "Har ikke førerkort",
+            fodselsdag: "2005-08-29",
+        },
+        {
+            navn: "Barack Hussein ObamaCare II",
+            kjonn: "Mann",
+            stilling: "Best President in the world",
+            telefonnummer: "Har ikke telefon",
+            valgfri: "I landed Michell Obama",
+            forerkort: "Blir kjørt i limousin",
+            fodselsdag: "1961-08-04",
+        }
+    ]
+};
